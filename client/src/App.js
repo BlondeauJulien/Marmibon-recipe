f20ab9d5a23844dfdb532cdd27a6ae369056560f
Julien Blondeau <julienblondeau@live.fr>
2019-10-29 22:37:23 +0100

    Create NavBar Component + styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './components/layout/NavBar';
 import './App.css';
 
 function App() {
-  return (
-   <h1>Hello</h1>
-  );
+	return (
+		<Fragment>
+			<Router>
+				<NavBar />
+			</Router>
+		</Fragment>
+	);
 }
 
 export default App;
---
+++ client/src/components/layout/NavBar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+	return (
+		<header>
+			<div className="header-top-container">
+				<Link className="logo-container brand-color-txt">
+					<i className="fas fa-cookie-bite" />
+					<h1>marmibon</h1>
+				</Link>
+				<div className="search-bar-container">
+					<i className="fas fa-search brand-color-txt" />
+					<input type="search" placeholder="Je cherche une recette:" />
+				</div>
+				<div className="header-btn-container">
+					<Link className="btn btn-mid btn-brand brand-color-bg text-color-white">
+						<i className="fas fa-user" />
+						{` `}Connexion
+					</Link>
+				</div>
+			</div>
+
+			<nav className="header-nav">
+				<ul>
+					<li>
+						<a href="#">Voir Tout</a>
+					</li>
+					<li>
+						<a href="#">Entrées</a>
+					</li>
+					<li>
+						<a href="#">Plats</a>
+					</li>
+					<li>
+						<a href="#">Desserts</a>
+					</li>
+					<li className="random-recipe">
+						<a href="#">Recette au hasard</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default NavBar;
