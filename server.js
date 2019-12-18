fa140a3c6ffe0c412a9f57e1fd5ef41f1cb03c1b
Julien Blondeau <julienblondeau@live.fr>
2019-12-18 18:34:28 +0100

    add process.env.PORT for heroku deploy

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/recipes', require('./routes/recipes')); 
 
 //Server static assets in production
-if(process.env.NODE_ENV=== 'production') {
+if(process.env.NODE_ENV === 'production') {
     // set static folder
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
